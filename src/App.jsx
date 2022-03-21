97cd7337cc4498398390b572b5fc1983bf317f2b
cjtrahey <trahey.03@gmail.com>
2022-03-21 18:38:21 -0400

    locator added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import "./app.scss";
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const[menuOpen,setMenuOpen] = useState(false);
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
