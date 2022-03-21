7594fc9fa2310272c78429e708b205f401d75785
cjtrahey <trahey.03@gmail.com>
2022-03-21 16:32:55 -0400

    smooth scrolling

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,12 @@ import "./topbar.scss";
 export default function Topbar() {
   return (
     <div className="topbar">
-
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">words</a>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
   )
 }
