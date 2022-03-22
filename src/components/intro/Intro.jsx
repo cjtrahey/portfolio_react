669e7485529c5debc4e279e6a27aef8fddbc96cb
cjtrahey <trahey.03@gmail.com>
2022-03-21 20:15:46 -0400

    making progress

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,25 @@
 import "./intro.scss";
+import { init } from 'ityped';
 
 export default function Intro() {
+  
   return (
     <div className="intro" id="intro">
-        test
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/man.png" alt=""/>
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi there, I'm</h2>
+            <h1>CJ Trahey</h1>
+            <h3>Freelance <span>Designer</span></h3>
+          </div>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt=""/>
+          </a>
+        </div>
     </div>
   )
 }
