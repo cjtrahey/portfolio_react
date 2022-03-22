9fa8b3e0c03f70fdc53fb2fc8528a7ba1a2b9eb2
cjtrahey <trahey.03@gmail.com>
2022-03-21 21:48:20 -0400

    intro finished

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,20 @@
 import "./intro.scss";
 import { init } from 'ityped';
+import { useEffect, useRef} from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect (()=> {
+    init(textRef.current, {
+       showCursor: false,
+       backDelay: 1500,
+       backSpeed: 60,
+       showCursor: true,
+        strings: ['Developer', 'Front-End Developer', 'Full-Stack Developer', 'Pokemon Fan' ],
+       });
+  },[])
   
   return (
     <div className="intro" id="intro">
@@ -14,7 +27,7 @@ export default function Intro() {
           <div className="wrapper">
             <h2>Hi there, I'm</h2>
             <h1>CJ Trahey</h1>
-            <h3>Freelance <span>Designer</span></h3>
+            <h3>I'm a <span ref={textRef}></span></h3>
           </div>
           <a href="#portfolio">
             <img src="assets/down.png" alt=""/>
