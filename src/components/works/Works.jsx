c68d3389ba35b4667732f0843a594baeb90b5a31
cjtrahey <trahey.03@gmail.com>
2022-03-22 23:14:26 -0400

    work started

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,27 @@ import "./works.scss";
 export default function Works() {
   return (
     <div className="works" id="works">
-      
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt=""/>
+                </div>
+                <h2>Title</h2>
+                <p>lorem ipsum dolor sit amet..
+                </p>
+                <span>Projects</span>
+              </div>
+
+            </div>
+            <div className="right">
+              <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt=""/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
