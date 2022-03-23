1ab43e8e39e085c3910c3aa90aae14a3fd57b6fe
cjtrahey <trahey.03@gmail.com>
2022-03-22 22:48:50 -0400

    using a goose to patch broken test links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import { 
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+ } from "../../data";
 
 export default function Portfolio() {
 
+  const [selected,setSelected] = useState("featured");
+  const [data,setData] = useState([]);
   const list = [
     {
       id: "featured",
@@ -26,46 +36,56 @@ export default function Portfolio() {
     },
   ];
 
+  useEffect(()=> {
+
+    switch(selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+        default:
+          setData(featuredPortfolio);
+    }
+
+  },[selected])
+
   return (
     <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <ul>
           {list.map(item=>(
-            <PortfolioList title={item.title}/>
+            <PortfolioList 
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+            />
           ))}
         </ul>
         <div className="container">
+          {data.map(d=>(
+
+
           <div className="item">
-            <img src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY.jpg" alt=""
+            <img
+            src={d.img}
+            alt=""
             />
-            <h3>Goose App</h3>
+            <h3>{d.title}</h3>
           </div>
-          <div className="item">
-            <img src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY.jpg" alt=""
-            />
-            <h3>Goose App</h3>
-          </div>
-          <div className="item">
-            <img src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY.jpg" alt=""
-            />
-            <h3>Goose App</h3>
-          </div>
-          <div className="item">
-            <img src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY.jpg" alt=""
-            />
-            <h3>Goose App</h3>
-          </div>
-          <div className="item">
-            <img src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY.jpg" alt=""
-            />
-            <h3>Goose App</h3>
-          </div>
-          <div className="item">
-            <img src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY.jpg" alt=""
-            />
-            <h3>Goose App</h3>
-          </div>
+        ))}
         </div>
     </div>
-  )
+  );
 }
--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -1,8 +1,8 @@
 import "./portfolioList.scss";
 
-export default function PortfolioList({title}) {
+export default function PortfolioList({ id,title,active,setSelected }) {
   return (
-    <li className="portfolioList">
+    <li className={active ? "portfolioList active" : "portfolioList"} onClick={() => setSelected(id)}>
         {title}
     </li>
   )
