5e3fad7404a9148dc9054891ad8f3c3465b23ce3
Cj Trahey <cjtrahey@Cjs-MacBook-Pro.local>
2022-04-11 17:59:21 -0400

    minor updates

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -13,9 +13,9 @@ export default function Menu({ menuOpen, setMenuOpen }) {
             <li onClick={()=>setMenuOpen(false)}>
                 <a href="#works">Works</a>
             </li>
-            <li onClick={()=>setMenuOpen(false)}>
+            {/* <li onClick={()=>setMenuOpen(false)}>
                 <a href="#testimonials">Testimonials</a>
-            </li>
+            </li> */}
             <li onClick={()=>setMenuOpen(false)}>
                 <a href="#contact">Contact</a>
             </li>
